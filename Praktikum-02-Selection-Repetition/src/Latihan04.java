e68b8d4cc1fbaabb169180b6b071fe812ea98769
Angga P Harahap <114120852+AnggaHarahap@users.noreply.github.com>
2022-10-01 09:51:45 +0700

    Update Latihan04.java

--- Praktikum-02-Selection-Repetition/src/Latihan04.java
+++ Praktikum-02-Selection-Repetition/src/Latihan04.java
@@ -2,19 +2,63 @@ import java.util.Scanner;
 
 public class Latihan04 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System. in );
-        System.out.println("Masukkan angka");
-        int nilai = input.nextInt();
-        System.out.println("Faktor-faktornya:");
-        int tambah = 0;
-        for (int z = 0; z <= nilai; z++) {
-            tambah++;
-            if (nilai % tambah == 0) {
-                System.out.print(tambah + " ");
-                                        
-            }
-        }
-
+		int x;
+		String jwb;
+		Scanner input = new Scanner(System.in);
+		
+		do {
+		System.out.println("DAFTAR BILANGAN");
+		System.out.println("1. FACTORIAL ASC");
+		System.out.println("2. FACTORIAL DESC");
+		System.out.print("Masukkan pilihan : ");
+		x = input.nextInt();
+		
+		switch (x) {
+		case 1:
+			faktorial(input);
+			break;
+		case 2:
+			faktorial_desc(input);
+			break;
+		
+			default:
+				System.out.println("salah");
+		}
+		
+		System.out.println("\nApa ingin mengulang (Y/N) ? = ");
+		jwb = input.nextLine();
+		}while (jwb.equals("Y"));
+		
+//		for(int a = 1; a <= x; a++) {
+//			String k = (a == x) ? "" : ",";
+//			System.out.print(a + k);
+//		}
+	
 	}
-
+	
+	static void faktorial(Scanner input) {
+		int sum = 1;
+		
+		System.out.print("Masukkan angka = ");
+		int f = input.nextInt();
+		
+		for (int i = 1; i <= f; i ++) {
+			sum = sum * i;
+			String k = (i == f ) ? " = " + sum: " x ";
+			System.out.print(i + k);
+		}
+	}
+	
+	static void faktorial_desc(Scanner input) {
+		int sum = 1;
+		
+		System.out.print("Masukkan angka = ");
+		int f = input.nextInt();
+		
+		for (int i = f; i >= 1; i--) {
+			sum = sum * i;
+			String k = (i == 1 ) ? " = " + sum: " x ";
+			System.out.print(i + k);
+		}
+	}
 }
